e7a922249a951d74d033320c4ccb555ed6a5b4c1
sadams <shawn.adams@titanmedicalinc.com>
2022-09-09 11:13:12 -0400

    Sort of working.... need to figure out how to connect functions and not just static methods.

--- main.py
+++ main.py
@@ -5,13 +5,49 @@
 
 
 import sys
-from sample import Ui_MainWindow
-from PyQt6 import QtCore, QtGui, QtWidgets
+from sample import *
 
+"""
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
     window = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(window)
+
+    window.
+
+    def on_click():
+        print('Yo!')
+
     window.show()
     sys.exit(app.exec())
+"""
+
+
+class myApp(Ui_MainWindow):
+    def __init__(self, window):
+        self.setupUi(window)
+        self.pushButton.clicked.connect(self.whenClicked)
+        self.buttonBox.accepted.connect(self.clickOK)
+        self.buttonBox.rejected.connect(self.clickCancel)
+
+    @staticmethod
+    def whenClicked():
+        print('Yo!')
+
+    @staticmethod
+    def clickOK():
+        print('Yo mama!')
+
+    @staticmethod
+    def clickCancel():
+        print('Not yo mama!')
+
+
+app = QtWidgets.QApplication(sys.argv)
+MainWindow = QtWidgets.QMainWindow()
+
+ui = myApp(MainWindow)
+
+MainWindow.show()
+app.exec()
